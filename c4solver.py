f9203dc54b13e2f93b9f9ce89184cf1cd2e0a74d
igrek51 <igrek.s@o2.pl>
2019-04-02 23:10:47 +0200

    Checking horizontal and vertical win condition

--- c4solver.py
+++ c4solver.py
@@ -24,6 +24,10 @@ class Grid(object):
             return None
         return column[y]
 
+    def set(self, x: int, y: int, value: Optional[str]):
+        column = self.columns[x]
+        column[y] = value
+
     def print(self):
         print('+-' + '--' * BOARD_W + '+')
         for y in reversed(range(BOARD_H)):
@@ -37,13 +41,19 @@ class Grid(object):
         print('+-' + '--' * BOARD_W + '+')
         print('| ' + ' '.join([str(i) for i in range(BOARD_W)]) + ' |')
 
-    def to2d(self):
+    def to2d_xy(self):
         return [
             [self.get(x, y) for y in range(BOARD_H)]
             for x in range(BOARD_W)
         ]
 
-    def put(self, x: int, value: str):
+    def to2d_yx(self):
+        return [
+            [self.get(x, y) for x in range(BOARD_W)]
+            for y in range(BOARD_H)
+        ]
+
+    def put(self, x: int, value: Optional[str]):
         # print('putting {} into {} column'.format(value, x))
         column = self.columns[x]
         if len(column) >= BOARD_H:
@@ -60,12 +70,60 @@ class Grid(object):
                     grid.put(idx, cell)
         return grid
 
+    def winner(self) -> Optional[str]:
+        return WinChecker.winner(self)
+
+    def clone(self):
+        copy = Grid()
+        copy.columns = [column[:] for column in self.columns]
+        return copy
+
+
+class WinCondition(Exception):
+    def __init__(self, winner: str):
+        self.winner = winner
+
 
 class WinChecker(object):
     @staticmethod
-    def won(grid: Grid) -> Optional[str]:
+    def winner(grid: Grid) -> Optional[str]:
+        return WinChecker(grid)._winner()
+
+    def __init__(self, grid: Grid):
+        self.grid = grid
+        self.yx = grid.to2d_yx()
+
+    def _winner(self) -> Optional[str]:
+        try:
+            self._check_vertical()
+            self._check_horizontal()
+            self._check_diagonal()
+        except WinCondition as e:
+            return e.winner
+
+    def _check_horizontal(self):
+        for row in self.yx:
+            self._check_list(row)
+
+    def _check_vertical(self):
+        for column in self.grid.columns:
+            self._check_list(column)
+
+    def _check_diagonal(self):
         return None
 
+    @staticmethod
+    def _check_list(l):
+        last_disc = None
+        streak = 0
+        for e in l:
+            if last_disc is None or e != last_disc:
+                streak = 0
+                last_disc = e
+            if e:
+                streak += 1
+            if streak >= WIN_CONDITION:
+                raise WinCondition(e)
 
 
 def main():
--- test_c4solver.py
+++ test_c4solver.py
@@ -89,18 +89,107 @@ def test_parse_grid():
 '''.strip())
 
 def test_grid_to2d():
-    array = Grid.parse('''
+    grid = Grid.parse('''
 .......
 .......
 ......B
 ......B
 .B....A
 .A.A..B
-'''.strip()).to2d()
+'''.strip())
+    array = grid.to2d_xy()
     assert array[1][0] is DISC_A
     assert array[1][1] is DISC_B
     assert array[0][0] is None
 
+    array = grid.to2d_yx()
+    assert array[0][1] is DISC_A
+    assert array[1][1] is DISC_B
+    assert array[0][0] is None
 
-# --- Test Grid Validator
+def test_grid_clone():
+    grid1 = Grid.parse('''
+.......
+.......
+......B
+......B
+.B....A
+.A.A..B
+'''.strip())
+    grid2 = grid1.clone()
+    grid1.set(1, 0, DISC_B)
+
+    assert grid1.get(1, 0) is DISC_B
+    assert grid2.get(1, 0) is DISC_A
+    assert grid2.get(1, 2) is None
+
+
+# --- Test Win Checker
+
+def test_win_checker_none():
+    assert Grid().winner() is None
+    assert Grid.parse('''
+.......
+.......
+......B
+.A....B
+.B.BA.A
+.A.AB.B
+'''.strip()).winner() is None
+    
+def test_win_checker_vertical():
+    assert Grid.parse('''
+.......
+.......
+......A
+......A
+.B....A
+.A.A..A
+'''.strip()).winner() is DISC_A
+    
+def test_win_checker_horizontal():
+    assert Grid.parse('''
+.......
+.......
+......A
+......A
+.B.BBBB
+AA.ABBA
+'''.strip()).winner() is DISC_B
+    
+def test_win_checker_diagonal():
+    assert Grid.parse('''
+.......
+.......
+......A
+.....AA
+.B.BABB
+AA.ABBA
+'''.strip()).winner() is DISC_A
+
+def list_winner(l):
+    try:
+        WinChecker._check_list(l)
+    except WinCondition as e:
+        return e.winner
+
+def test_win_checker_streak_check():
+    assert list_winner([]) is None
+    assert list_winner([DISC_A]) is None
+    assert list_winner([DISC_A, DISC_A, DISC_A, DISC_A]) is DISC_A
+    assert list_winner([DISC_A, DISC_A, DISC_A, DISC_A, None, None]) is DISC_A
+    assert list_winner([None, None, DISC_A, DISC_A, DISC_A, DISC_A]) is DISC_A
+    assert list_winner([DISC_A, DISC_B, DISC_B, DISC_B, DISC_B]) is DISC_B
+    assert list_winner([None, DISC_A, DISC_A, None, DISC_B, DISC_B, DISC_B, DISC_B, None]) is DISC_B
+
+
+
+
+
+
+
+
+
+
+
 
